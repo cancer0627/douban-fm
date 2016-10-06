a2e894fe6481b8e7bd47a54ba7f3e4cbe45e3e37
cancer0627 <cancer_1942913930@163.com>
2016-10-06 10:21:49 +0800

    列表完成，样式优化，代码优化

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 (function() {
     var user_name = document.getElementById('user_name');
+    var list_date = document.getElementsByClassName('list_date')[0];
+    var date = new Date();
+    var day = date.getDate();
+    var mon = date.getMonth() + 1;
+
+    function match_date(a) {
+        if (a < 10) {
+            a = '0' + a;
+        }
+        return a;
+    }
+    list_date.childNodes[1].innerHTML = match_date(day);
+    list_date.childNodes[3].innerHTML = match_date(mon) + "月";
 }(window))
